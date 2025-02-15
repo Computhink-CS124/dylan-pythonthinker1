64534869eaa973ee364701d1d730faa61df5568c
Computhink-CS124 <cs124@computhink.com.sg>
2025-02-15 06:21:50 +0000

    Feb 15, 2025, 6:21 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -2,4 +2,4 @@ print("Hello from lesson 4")
 
 age = itr = (input("How old are you? "))
 age = age + 1
-print("Next year, you will be " + age + )
+print("Next year, you will be " + age + "years old.")
