74af7674e5bd241ad7439e4cf9670edd334646d5
Computhink-CS124 <cs124@computhink.com.sg>
2025-02-15 06:31:00 +0000

    Feb 15, 2025, 6:31 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,14 +1,1 @@
-print("Hello from lesson 4")
-
-age = int (input("How old are you? "))
-age = age + 1
-
-print("Next year, you will be " + str (age) + " years old.")\
-
-num = int(input("give me a random number "))
-num = num * 2
-print("double your number is "+ str (num) + ".")
-
-year = int (input("What year were u born? "))
-year = 2025 - year
-print("You are " + str(year) + " years old.")
+# 
