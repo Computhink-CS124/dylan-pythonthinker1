b6c04f800cb4bbfd7d97f45b9092a8d539d5d887
Computhink-CS124 <cs124@computhink.com.sg>
2025-02-22 07:27:49 +0000

    Feb 22, 2025, 7:27 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -19,35 +19,58 @@
 # for i in range(students):
 #     score = score + int(input("what is the score of the student "))
 # averagescore =(score / students )
-# print("the average score of the students is " + str(averagescore))
-for i in range(3):
-    print("Hello, World!")
+# # print("the average score of the students is " + str(averagescore))
+# for i in range(3):
+#     print("Hello, World!")
 
-# # **Task 1b**:
-# for i in range(5):
+# # # **Task 1b**:
+# # for i in range(5):
+# #     print(i)
+
+# # # **Task 1c**:
+# # print("Hello, World!")
+
+# # # **Task 1d**:
+# # i = 5
+
+# # #**Task 1e**:
+# # print("Hello, World!")
+
+# # **Task 2a**:
+# age = 10
+# print(age)
+
+# # **Task 2b**:
+# name = "Alice"
+# print(name)
+
+# # **Task 2c**:
+# x = 5
+# print(x)
+
+
+# # **Task 2d**:
+# print("Hello, World!")
+# **Task 3a**:
+# age = "25"
+# print(age + 1)
+
+# **Task 3b**:
+# number = 10
+# print(number - "5")
+
+# **Task 3c**:
+# print("Repeat" * "3")
+
+# **Task 3d**:
+# year = 2023
+# print("The year is " + year)
+
+# **Task 3e**:
+# x = "10"
+# y = x / 2
+
+# **Task 3f**:
+# end = "5"
+# for i in range(end):
 #     print(i)
-
-# # **Task 1c**:
-# print("Hello, World!")
-
-# # **Task 1d**:
-# i = 5
-
-# #**Task 1e**:
-# print("Hello, World!")
-
-# **Task 2a**:
-age = 10
-print(age)
-
-# **Task 2b**:
-name = "Alice"
-print(name)
-
-# **Task 2c**:
-x = 5
-print(x)
-
-
-# **Task 2d**:
-print("Hello, World!")
