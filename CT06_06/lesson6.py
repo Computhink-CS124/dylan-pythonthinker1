5592b0dc42ae87a3d51e4e4e6ca2417a8e370f58
Computhink-CS124 <cs124@computhink.com.sg>
2025-02-22 06:33:18 +0000

    Feb 22, 2025, 6:33 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,7 +1,7 @@
-#print("Hello from lesson 6")
-for count in range(2, 25, 2):
-    print(count)
-for count in range(8, 97, 8):
-    print(count)
-for count in range(5, 0, -1):
-    print(count)
+# #print("Hello from lesson 6")
+# for count in range(2, 25, 2):
+#     print(count)
+# for count in range(8, 97, 8):
+#     print(count)
+# for count in range(5, 0, -1):
+#     print(count)
