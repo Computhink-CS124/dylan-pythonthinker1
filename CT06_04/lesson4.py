ba7b5b915c1db1a6543cd168c71837e1a38133e8
Computhink-CS124 <cs124@computhink.com.sg>
2025-02-22 06:21:04 +0000

    Feb 22, 2025, 6:21 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,2 +1,12 @@
 # 
-age = int(input("what is u"))
+age = int(input("what is ur current age "))
+age = age + 1
+print("Next year, you will be " + str(age) + " years old.")
+
+num = int(input("give me a random number "))
+num = num * 2
+print("double your number is " + str(num) + ".")
+
+birthyear = int(input("what year were you born in "))
+birthyear = 2025 - birthyear
+print("you are " + str(birthyear) + " years old.")
--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,25 @@
-print("Hello from lesson 5")
+#print("Hello from lesson 5")
+# name = input("what is the birthday person's name? ")
+# age = input("what age is he/she turning this year? ")
+# message = input("What message do you want to send to them? ")
+# print("Happy " + age + "th birthday " + name + "! " + message)
+#for i in range(10):
+    #print("Hello!")
+# name = input("what is your name? ")
+
+
+# for (char) in name:
+#     print("Give me a " + char + "!")
+# print("What do we have?")
+# print(name + " is the best!")
+
+# for i in range(100):6
+#     print("I like cake")
+#     print("give me more")
+
+for count in range(1, 6):
+    print(count)
+for count in range(51, 101):
+    print(count)
+for count in range(18, 30):
+    print(count)
