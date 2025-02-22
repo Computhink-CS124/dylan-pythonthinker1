cd7151fc3a41444621801f9d3c25537096951845
Computhink-CS124 <cs124@computhink.com.sg>
2025-02-22 06:40:03 +0000

    Feb 22, 2025, 6:40 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -5,8 +5,8 @@
 #     print(count)
 # for count in range(5, 0, -1):
 #     print(count)
-start = int(input("what number do you want with start with?"))
-stop = int(input("what number do you want to end with" ))
+start = int(input("what number do you want with start with ?"))
+stop = int(input("what number do you want to end with "))
 if start > stop:
     stop = stop + 1
     for i in range(start, stop):
