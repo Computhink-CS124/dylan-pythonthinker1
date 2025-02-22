19887bd4c194c89840ecbbffa54f3ee853abc82e
Computhink-CS124 <cs124@computhink.com.sg>
2025-02-22 07:15:42 +0000

    Feb 22, 2025, 7:15 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -22,3 +22,16 @@
 # print("the average score of the students is " + str(averagescore))
 for i in range(3):
     print("Hello, World!")
+
+# **Task 1b**:
+for i in range(5):
+    print(i)
+
+# **Task 1c**:
+print("Hello, World!")
+
+# **Task 1d**:
+i = 5
+
+#**Task 1e**:
+print("Hello, World!")
