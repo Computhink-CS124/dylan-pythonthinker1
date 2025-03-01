5e2d279bd886467130a2e6551a3c7c2d80822934
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-01 07:25:31 +0000

    Mar 1, 2025, 7:25 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -9999,4 +9999,4 @@ print("Hello from lesson 7")
 # print("the sum of the numbers is " + str(sum))
 n = int(input("What number for the timestable "))
 for i in range(1, 13):
-    print(str(n) + " x " + str(i) + "= " + (n * i))
+    print(str(n) + " x " + str(i) + "= " + str(n * i))
