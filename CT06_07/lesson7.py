d51f71b56fb41c02a93c2396ec4d513cd4fcb99b
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-01 06:39:36 +0000

    Mar 1, 2025, 6:39 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,12 +1,15 @@
 print("Hello from lesson 7")
 
-score_one = 90
-score_two = 80
-score_three = 75
+# score_one = 90
+# score_two = 80
+# score_three = 75
 
-total = score_one + score_two + score_three
+# total = score_one + score_two + score_three
 
-average_score = total / 3
+# average_score = total / 3
 
-student_name = "Alex"
-print("Average score for " + student_name + " is: " + str(average_score))
+# student_name = "Alex"
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+
+
