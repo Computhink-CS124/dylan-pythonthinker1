f3fcb42b6d13b3acd8d813b23758d143a84cc740
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-01 07:13:25 +0000

    Mar 1, 2025, 7:13 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -9995,21 +9995,15 @@ print("Hello from lesson 7")
 # for i in range(10, 0, -1):
     # print(i)
 
-
-
 # word = (input("What word would you like to repeat? "))
 # n = int(input("how many times would you like to repeat it for? "))
 # for i in range(n):
 #     print(word)
-
 # name = input("what is ur name? ")
 # n = int(input("how many times would you like to repeat? "))
 # for i in range(n):
 #     print ("Nice to meet you " + name)
-
-
 sum = 0
 for n in range(1, 6):
     sum = sum + int(input("what is number #" + str(n)))
-
 print("the sum of the numbers is " + str(sum))
