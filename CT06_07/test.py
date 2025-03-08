00f01d9becc6b6e9a2f641ce565f9170c3ab8211
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-08 06:43:05 +0000

    Mar 8, 2025, 6:43 AM

--- CT06_07/test.py
+++ CT06_07/test.py
@@ -1,10 +1,1 @@
-start = int(input("What number do you want to start with"))  #asking the user what number they want to start with
-end = int(input("What number do you want to end with"))  #asking the user what number they want to end with
-increment = int(input("What number is the increment "))  #asking the user for the increment
-if start > end:
-    for i in range(start, end - 1, increment):  #making a loop
-        print(i)  #printing the answer
-else:
-    for i in range(start, end + 1, increment):  #making a loop
-        print(i)  #printing the answer
-
+2apples = hawtuah
