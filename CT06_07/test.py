0090650fc180bd4686aaee9b32e14461e6dec9f7
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-08 06:34:46 +0000

    Mar 8, 2025, 6:34 AM

--- CT06_07/test.py
+++ CT06_07/test.py
@@ -2,4 +2,4 @@ start = int(input("What number do you want to start with"))  #asking the user wh
 end = int(input("What number do you want to end with"))  #asking the user what number they want to end with
 increment = int(input("What number is the increment "))  #asking the user for the increment
 for i in range(start, end, increment):  #
-    print (i)  #printing the r
+    print (i)  #printing the result
