94950939ffccf21b2dd6f8a93682fd17c04bf34a
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-08 06:30:41 +0000

    Mar 8, 2025, 6:30 AM

--- CT06_07/test.py
+++ CT06_07/test.py
@@ -1,3 +1,3 @@
 start = input("What number do you want to start with")#asking the user what number they want to start with
-end = input("What number do you want to start with")
+end = input("What number do you want to start with")#asking the user what number they want to end with
 increment = input("")
