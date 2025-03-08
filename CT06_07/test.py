e577f94e4b5c2cab07d0eff12379512f73f5c12b
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-08 06:25:13 +0000

    Mar 8, 2025, 6:25 AM

--- CT06_07/test.py
+++ CT06_07/test.py
@@ -1,2 +1,2 @@
 name = input("What is your name? ") #asking the person for their name
-print("Nice to meet  you " + name + "!") #printing the 
+print("Nice to meet  you " + name + "!") #printing the persons name
