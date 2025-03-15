fb2ebec8216845155962c2b1e0d9b1006bc96111
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-15 06:26:26 +0000

    Mar 15, 2025, 6:26 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -25,7 +25,8 @@ print("Hello from lesson 7")
 
 sum = 0
 for i in range(1, 6):
-    sum = sum + int(input("What is the score of student#" + ))
+    sum = sum + int(input("What is the score of student#" + i))
+
 
 
 
