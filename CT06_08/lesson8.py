92321caec8042408e64bd6e04d1c89c3c1598afa
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-15 06:56:10 +0000

    Mar 15, 2025, 6:56 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,6 +1,6 @@
 print("Hello from lesson 8")
 import time
-sec = int(ip)
+sec = int(input("How many seconds do you want to countdown from? "))
 for i in range(10, 0, -1):
     print(i)
     time.sleep(1)
