5237a3ed540c260778dbfe8560351f56c42794d1
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-15 06:27:07 +0000

    Mar 15, 2025, 6:27 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -27,7 +27,7 @@ sum = 0
 for i in range(1, 6):
     sum = sum + int(input("What is the score of student#" + i))
 average_score = sum / 5
-print("The average score of your students is")
+print("The average score of your students is " + average_score)
 
 
 
