4b0bb93063c795f4f770a4ce9bed8923c378319a
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-15 06:57:14 +0000

    Mar 15, 2025, 6:57 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -4,4 +4,4 @@ sec = int(input("How many seconds do you want to countdown from? "))
 for i in range(sec, 0, -1):
     print(i)
     time.sleep(1)
-print ("happy new year!")
+print ("the bomb exploded!")
