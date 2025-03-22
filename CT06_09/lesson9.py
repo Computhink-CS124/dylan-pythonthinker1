cfa7baa642da07d13692150b1ec69908662eeec0
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-22 06:49:57 +0000

    Mar 22, 2025, 6:49 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -5,4 +5,4 @@ num3 = int(input("What is the third number? "))
 
 print("First num: " + str(num1))
 print("Second num: " + str(num2) )
-print("Thired num: " + str(num3))
+print("Third num: " + str(num3))
