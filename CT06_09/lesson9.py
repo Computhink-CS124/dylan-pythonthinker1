ce0015a2f09d2f2ee74165d52ab97693a23b4a43
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-22 06:51:10 +0000

    Mar 22, 2025, 6:51 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -6,3 +6,7 @@ num3 = int(input("What is the third number? "))
 print("First num: " + str(num1))
 print("Second num: " + str(num2) )
 print("Third num: " + str(num3))
+
+num1iseven = num1 % 2 == 0 
+num2iseven = num2 % 2 == 0
+num3iseven = num3 % 2 == 0
