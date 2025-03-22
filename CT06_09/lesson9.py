93493382ed020df377d22ab19b079fd2465049d3
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-22 06:30:50 +0000

    Mar 22, 2025, 6:30 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -3,4 +3,4 @@ num = int(input("How many questions do you want? "))
 for i in range(num):
     num1 = random.randint(1, 10)
     num2 = random.randint(1, 10)
-    ans = int(input("What is"))
+    ans = int(input("What is " + (num1) + "x"))
