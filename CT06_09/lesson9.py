6e3ba6a1413888aea025c1734e784e76e215dcfc
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-22 06:48:32 +0000

    Mar 22, 2025, 6:48 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,4 +1,8 @@
 import random
 num1 = int(input("What is the first number? "))
 num2 = int(input("What is the second number? "))
-num3 = int(input("What is the third number? "))  
+num3 = int(input("What is the third number? ")) 
+
+print("First num: " + str(num1))
+print("Second num: " + str(num2) )
+all_even_odd = num1
