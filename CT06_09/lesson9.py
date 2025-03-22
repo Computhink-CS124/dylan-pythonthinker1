4332ad5d1de5dd4546b2e1d0953a20fda5179fa2
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-22 07:05:26 +0000

    Mar 22, 2025, 7:05 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,14 +1,14 @@
-import random
-num1 = int(input("What is the first number? "))
-num2 = int(input("What is the second number? "))
-num3 = int(input("What is the third number? ")) 
+# import random
+# num1 = int(input("What is the first number? "))
+# num2 = int(input("What is the second number? "))
+# num3 = int(input("What is the third number? ")) 
 
-print("First num: " + str(num1))
-print("Second num: " + str(num2) )
-print("Third num: " + str(num3))
+# print("First num: " + str(num1))
+# print("Second num: " + str(num2) )
+# print("Third num: " + str(num3))
 
-num1iseven = num1 % 2 == 0 
-num2iseven = num2 % 2 == 0
-num3iseven = num3 % 2 == 0
-all_even_odd = num1iseven == num2iseven == num3iseven
-print(all_even_odd)
+# num1iseven = num1 % 2 == 0 
+# num2iseven = num2 % 2 == 0
+# num3iseven = num3 % 2 == 0
+# all_even_odd = num1iseven == num2iseven == num3iseven
+# print(all_even_odd)
