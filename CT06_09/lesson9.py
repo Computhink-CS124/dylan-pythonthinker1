691fb660a942727d82001e6d85a5934441be6a37
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-22 06:30:09 +0000

    Mar 22, 2025, 6:30 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,2 +1,5 @@
 import random
-num = int(input("How many questions do you want "))
+num = int(input("How many questions do you want? "))
+for i in range(num):
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
