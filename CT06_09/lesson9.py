f62245b365a55d170604da6645ebd94237277e52
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-22 06:22:21 +0000

    Mar 22, 2025, 6:22 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -2,4 +2,5 @@ import random
 guess = int(input("Guess a number from 1 to 10 "))
 num1 = random.randint(1, 10)
 print("Guess: "+ str(guess))
-print("num1: " + str())
+print("num1: " + str(num1))
+print(num1 == guess)
