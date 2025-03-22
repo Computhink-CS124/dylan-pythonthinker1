98e9754f9ab7288fc84d33431fcdb6e8be2fc2cb
Computhink-CS124 <cs124@computhink.com.sg>
2025-03-22 06:46:40 +0000

    Mar 22, 2025, 6:46 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,4 +1,6 @@
 import random
 num1 = int(input("What is the first number? "))
 num2 = int(input("What is the second number? "))
-num3 = int(input("What is the third number? "))
+num3 = int(input("What is the third number? "))  
+for i in range(3):
+    ans1
