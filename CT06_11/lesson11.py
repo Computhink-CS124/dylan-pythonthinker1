2d263fa18613d95bed01825bc5b8a4fd9272f826
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-05 07:28:16 +0000

    Apr 5, 2025, 7:28 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -53,4 +53,6 @@ pwinput = input("What is pw")
 if usernameinput == username and pwinput == pw:
     print("Acces granted!")
 elif usernameinput == username or pwinput == pw:
-    print
+    print("either username or pw is incorrect")
+else:
+    print("")
