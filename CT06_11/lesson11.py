f52e7a3206c3e001b9d4fa104cff250feb1fc9f5
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-05 06:25:55 +0000

    Apr 5, 2025, 6:25 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,12 +1,12 @@
-print("Hello from lesson 11")
-px = int(input("price "))
-if px <= 5:
-    print("Sound good!")
-elif px <= 50:
-    print ("r u sure we need this?")
-elif px <= 500:
-    print("Where are u getting all this money from?")
-else: 
-    print("DONT U DARE BUY THIS EXPENSIVE AHH SKIBIDI TOILET!!!")
-if px <= 50000:
-    print ("NOOOOOOOOOOOOOO WERE GOING BACK TO THE SLUMS")
+# print("Hello from lesson 11")
+# px = int(input("price "))
+# if px <= 5:
+#     print("Sound good!")
+# elif px <= 50:
+#     print ("r u sure we need this?")
+# elif px <= 500:
+#     print("Where are u getting all this money from?")
+# else: 
+#     print("DONT U DARE BUY THIS EXPENSIVE AHH SKIBIDI TOILET!!!")
+# if px <= 50000:
+#     print ("NOOOOOOOOOOOOOO WERE GOING BACK TO THE SLUMS")
