0cf00fc049fc1e84e00b782b8537d7d0f9842056
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-12 06:47:27 +0000

    Apr 12, 2025, 6:47 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -6,11 +6,13 @@
 
 
 
-visitors = 0
-max = 30
-while visitors < max:
-    add = (input("do u wanna add a visiititiros????? "))
-    if add == "yes":
-        visitors = visitors + 1
-        print("ok  i will add visitor")
-        print("there are " + str(visitors) + " visiititiros now")
+# visitors = 0
+# max = 30
+# while visitors < max:
+#     add = (input("do u wanna add a visiititiros????? "))
+#     if add == "yes":
+#         visitors = visitors + 1
+#         print("ok  i will add visitor")
+#         print("there are " + str(visitors) + " visiititiros now")
+
+
