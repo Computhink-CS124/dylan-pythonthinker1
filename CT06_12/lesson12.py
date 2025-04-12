5f6e48adfb88071046caa6cd40d3a18cce5bec21
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-12 06:38:34 +0000

    Apr 12, 2025, 6:38 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -13,4 +13,4 @@ while visitors < max:
     if add == "yes":
         visitors = visitors + 1
         print("ok  i will add visitor")
-        print("there are " + visitors + " ")
+        print("there are " + visitors + " visiititiros now")
