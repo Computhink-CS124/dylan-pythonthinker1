cb60b8571aaa521718b3cbedeeec602acf7b5bd5
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-12 06:09:12 +0000

    Apr 12, 2025, 6:09 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -65,4 +65,4 @@ num = int(input("Give me a number "))
 if num % 3 == 0 and num % 5 == 0:
     print("The number is divisable by 3 and 5")
 else:
-    
+    print("The number is not divisable by 3 and 5")
