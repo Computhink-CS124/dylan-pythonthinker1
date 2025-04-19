0b1563727946bdc13e591e2e15166a9fe5801886
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-19 06:30:01 +0000

    Apr 19, 2025, 6:30 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -13,7 +13,7 @@ while True:
     if userans == 2:
         amooount = int(input("HOW MUCH "))
         balance = balance + amooount
-        print ("")
+        print ("OKEY  DONE YOU GOT " + str(balance))
     if userans == 3:
         print(str(balance))
     if userans == 4:
