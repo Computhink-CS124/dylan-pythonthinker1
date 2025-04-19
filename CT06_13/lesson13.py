8e0603e782da4d5dee74b20e0bad1e891d34d507
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-19 06:26:38 +0000

    Apr 19, 2025, 6:26 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,7 +1,9 @@
 # print("Hello from lesson 13")
 
-
+balance = 1000
 while True:
     userans = int(input("WHAT DO U WANT DO DO ONE WITHDRAW TWO DEPOSIT TREE CHECK HOW MUCH MOOLAH FOUR EXITTT"))
     if userans == 1:
-        amoooount = int(input(""))
+        amoooount = int(input("HWO MUCH MONEY  "))
+        if amoooount <= balance:
+            print("OKEY ")
