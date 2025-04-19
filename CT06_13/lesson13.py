46e47e9bba54cbac9a92a096eae070b89265328f
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-19 06:47:21 +0000

    Apr 19, 2025, 6:47 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,20 +1,23 @@
-# print("Hello from lesson 13")
+# # print("Hello from lesson 13")
 
-balance = 1000
-while True:
-    userans = int(input("WHAT DO U WANT DO DO ONE WITHDRAW TWO DEPOSIT TREE CHECK HOW MUCH MOOLAH FOUR EXITTT"))
-    if userans == 1:
-        amoooount = int(input("HWO MUCH MONEY  "))
-        if amoooount <= balance:
-            balance = balance - amoooount
-            print("OKEY  DONE YOU GOT " + str(balance))
-        else:
-            print("YOU BROKE BOIII U A FAILUREEEEE ")
-    if userans == 2:
-        amooount = int(input("HOW MUCH "))
-        balance = balance + amooount
-        print ("OKEY  DONE YOU GOT " + str(balance))
-    if userans == 3:
-        print(str(balance))
-    if userans == 4:
-        break
+# balance = 1000
+# while True:
+#     userans = int(input("WHAT DO U WANT DO DO ONE WITHDRAW TWO DEPOSIT TREE CHECK HOW MUCH MOOLAH FOUR EXITTT"))
+#     if userans == 1:
+#         amoooount = int(input("HWO MUCH MONEY  "))
+#         if amoooount <= balance:
+#             balance = balance - amoooount
+#             print("OKEY  DONE YOU GOT " + str(balance))
+#         else:
+#             print("YOU BROKE BOIII U A FAILUREEEEE ")
+#     if userans == 2:
+#         amooount = int(input("HOW MUCH "))
+#         balance = balance + amooount
+#         print ("OKEY  DONE YOU GOT " + str(balance))
+#     if userans == 3:
+#         print(str(balance))
+#     if userans == 4:
+#         break
+
+
+
