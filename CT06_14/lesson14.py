7cdaa5e9cc0f2d482f7000ddab29663d033179c7
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-26 06:52:47 +0000

    Apr 26, 2025, 6:52 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,25 +1,25 @@
-# print("Hello from lesson 15")
-i = 0
-pisatopping = [
-"mushrooms", 
-"pepperoni",
-"cheese"
-]
-userinput = []
+# # print("Hello from lesson 15")
+# i = 0
+# pisatopping = [
+# "mushrooms", 
+# "pepperoni",
+# "cheese"
+# ]
+# userinput = []
 
-for item in pisatopping:
-    print(str(i + 1) + str(pisatopping[i]))
-    i = i + 1
+# for item in pisatopping:
+#     print(str(i + 1) + str(pisatopping[i]))
+#     i = i + 1
 
-while True:
+# while True:
     
-    usertop = (input("Wut topping u want?????"))
-    # userinput.append = pisatopping[usertop - 1]
-    if userinput == "end":
-        break
-    else:
-        userinput.append(pisatopping[int(i) - 1])
+#     usertop = (input("Wut topping u want?????"))
+#     # userinput.append = pisatopping[usertop - 1]
+#     if userinput == "end":
+#         break
+#     else:
+#         userinput.append(pisatopping[int(i) - 1])
 
-for item in userinput:
-    print(item)
-print(userinput)
+# for item in userinput:
+#     print(item)
+# print(userinput)
