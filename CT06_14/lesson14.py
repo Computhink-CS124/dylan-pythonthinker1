c8335d27d761e5519dce8fba27798d309115cda9
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-26 06:42:19 +0000

    Apr 26, 2025, 6:42 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -11,7 +11,7 @@ while True:
     for item in pisatopping:
         print(str(i + 1) + str(pisatopping[i]))
         i = i + 1
-    int(input("Wut topping u want?????"))
+    usertop = int(input("Wut topping u want?????"))
     userinput.append = pisatopping[int(input("Wut topping u want?????")) - 1]
     if userinput == "end":
             break
