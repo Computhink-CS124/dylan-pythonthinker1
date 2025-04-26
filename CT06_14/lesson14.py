57a63313ef9aceecafa5d7bf8af547d5087ad951
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-26 07:27:30 +0000

    Apr 26, 2025, 7:27 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -49,10 +49,10 @@ for i in range(4):
 
 for i in range(3):
     t.forward(180)
-    t.right(1)
+    t.right(120)
 
-for i in range(360):
-    t.forward(1)
+for i in range(5):
+    t.forward()
     t.right(1)
 
 
