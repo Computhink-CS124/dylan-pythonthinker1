0f92cb1625f5091afc3e02282280e66201510815
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-26 06:33:17 +0000

    Apr 26, 2025, 6:33 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -8,6 +8,6 @@ pisatopping = [
 while True:
     for n in len:
         userinput = int(input("Wut topping u want " + ([n + 1] + pisatopping[n] )))
-        if userinout == "end":
+        if userinput == "end":
             break
 print( )
