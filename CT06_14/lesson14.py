2c579b09ea961fac7475258811e554f16c4a98ee
Computhink-CS124 <cs124@computhink.com.sg>
2025-04-26 06:38:02 +0000

    Apr 26, 2025, 6:38 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,13 +1,16 @@
 # print("Hello from lesson 15")
-
+i = 0
 pisatopping = [
 "mushrooms", 
 "pepperoni",
 "cheese"
 ]
+userinput = []
 while True:
-    for i in ohio:
-        userinput = int(input("Wut topping u want " + ([n + 1] + pisatopping[n] )))
+    for item in pisatopping:
+        userinput.append = int(input("Wut topping u want " + ([i + 1] + pisatopping[i] )))
         if userinput == "end":
             break
+        else:
+            userinput.append(pisatopping[int(userinput) - 1])
 print( )
