ad53f084d988e3412bd23c8ba751f042284325da
Computhink-CS124 <cs124@computhink.com.sg>
2025-05-10 07:01:43 +0000

    May 10, 2025, 7:01 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -1,10 +1,1 @@
-planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
-print(planets[2])
-planets.append ("neptune")
-planets[3] = "muskworld"
-planets.pop(6)
-length = len(planets)
-counter = 0
-for i in range (length):
-    print (planets[counter])
-    counter = counter + 1
+word = 
