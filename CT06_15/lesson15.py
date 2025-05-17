0caaf9314594d6c2dd0f3288970c2f988fdc712e
Computhink-CS124 <cs124@computhink.com.sg>
2025-05-17 07:06:35 +0000

    May 17, 2025, 7:06 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -2,7 +2,7 @@ def isEven():
     n % 2 == 0
 numbers = [3, 9, 2, 4, 5, 8]
 for n in numbers:
-    if isEven == True:
+    if isEven() == True:
         print (str(n) + " is an even number")
     else:
         print (str(n) + " is an odd number")
