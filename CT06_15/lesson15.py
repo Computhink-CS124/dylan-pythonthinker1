94bb3d959a7ca1eee0a68dba2fc413c028e8a1f1
Computhink-CS124 <cs124@computhink.com.sg>
2025-05-17 07:15:45 +0000

    May 17, 2025, 7:15 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -1,8 +1,11 @@
-def isEven(num):
-    return num % 2 == 0
-numbers = [3, 9, 2, 4, 5, 8]
-for n in numbers:
-    if isEven(n):
-        print (str(n) + " is an even number")
-    else:
-        print (str(n) + " is an odd number")
+# def isEven(num):
+#     return num % 2 == 0
+# numbers = [3, 9, 2, 4, 5, 8]
+# for n in numbers:
+#     if isEven(n):
+#         print (str(n) + " is an even number")
+#     else:
+#         print (str(n) + " is an odd number")
+
+def square(num):
+    
