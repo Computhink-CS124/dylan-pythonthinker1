c99cdcfd795f521ca1570024b5722e8ce857e847
Computhink-CS124 <cs124@computhink.com.sg>
2025-05-24 06:39:01 +0000

    May 24, 2025, 6:39 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -1,7 +1,7 @@
 # print("Hello from lesson 16")
 import turtle
-window=turtle.Screen()
-t=turtle.Turtle()
+# window=turtle.Screen()
+# t=turtle.Turtle()
 
 
 def setup_screnn (screenWidth, screenHeight):
