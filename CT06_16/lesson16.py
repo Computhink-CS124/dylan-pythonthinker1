a88024596c120e2b16ae1c8cb882117ebca91413
Computhink-CS124 <cs124@computhink.com.sg>
2025-05-24 06:32:19 +0000

    May 24, 2025, 6:32 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -3,12 +3,8 @@ import turtle
 window=turtle.Screen()
 t=turtle.Turtle()
 
-def drawshape (length, numsides):
-    for i in range(numsides):
-        t.forward(length)
-        t.left(360 / numsides)
-lengthvar = int(input("length? "))
-drawshape(lengthvar, 4)
+
+
 
 
 
